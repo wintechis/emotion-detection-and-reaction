16b5da126959ee1cbbedfef2dfdb8add16873d9c
JanK <j.kuehlborn@gmx.net>
2022-06-30 15:42:58 +0200

    start implementing multiprocessing

--- webapp/app.py
+++ webapp/app.py
@@ -1,26 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import threading
+from recognizer import *
 
 app = Flask(__name__)
+video = cv2.VideoCapture(0)
 
-def analyze_webcam():
-    pass
 
-def analyze_audio():
-    pass
+def gen(video):
+    while True:
+        success, image = video.read()
+        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.equalizeHist(frame_gray)
+
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video), mimetype='multipart/x-mixed-replace; boundary=frame')   # gets called from recognizer.html
+
+
 @app.route('/recognizer')
 def recognizer():
+    # return emotions and post them to html
+    global video
+    '''
+    start process1
+    start process2
+    
+    '''
+    while True:
+        for time <= 3 sekunden
+            await get video predictions
+
+        await get audio predicitons
+
+
+
     return render_template('recognizer.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ webapp/recognizer.py
@@ -0,0 +1,127 @@
+from tensorflow import keras
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa
+from sklearn.preprocessing import StandardScaler
+import pyaudio
+import wave
+import time
+
+scaler = StandardScaler()
+#model_audio = keras.models.load_model('models/audio_model_small.h5')
+
+
+# ToDo include following small functions in extract_audio
+
+def noise(data):
+    noise_amp = 0.035*np.random.uniform()*np.amax(data)
+    data = data + noise_amp*np.random.normal(size=data.shape[0])
+    return data
+
+
+def stretch(data, rate=0.8):
+    return librosa.effects.time_stretch(data, rate)
+
+
+def shift(data):
+    shift_range = int(np.random.uniform(low=-5, high = 5)*1000)
+    return np.roll(data, shift_range)
+
+
+def pitch(data, sampling_rate, pitch_factor=0.7):
+    return librosa.effects.pitch_shift(data, sampling_rate, pitch_factor)
+
+
+# main functions
+
+def extract_audio_features(data, sample_rate):
+    # ZCR
+    result = np.array([])
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    result = np.hstack((result, zcr))  # stacking horizontally
+
+    # Chroma_stft
+    stft = np.abs(librosa.stft(data))
+    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, chroma_stft))  # stacking horizontally
+
+    # MFCC
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mfcc))  # stacking horizontally
+
+    # Root Mean Square Value
+    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+    result = np.hstack((result, rms))  # stacking horizontally
+
+    # MelSpectogram
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mel))  # stacking horizontally
+
+    return result
+
+
+def get_audio_features(path):
+    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
+    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+
+    # without augmentation
+    res1 = extract_audio_features(data, sample_rate)
+    result = np.array(res1)
+
+    # data with noise
+    noise_data = noise(data)
+    res2 = extract_audio_features(noise_data, sample_rate)
+    result = np.vstack((result, res2))  # stacking vertically
+
+    # data with stretching and pitching
+    new_data = stretch(data)
+    data_stretch_pitch = pitch(new_data, sample_rate)
+    res3 = extract_audio_features(data_stretch_pitch, sample_rate)
+    result = np.vstack((result, res3))  # stacking vertically
+
+    return result
+
+
+# driver function: returns prediction
+
+def analyze_audio():
+    samp_rate = 44100  # 44.1kHz sampling rate
+    chunk = 4096  # 2^12 samples for buffer
+    record_secs = 3  # seconds to record
+    dev_index = 1  # device index found by p.get_device_info_by_index(ii)
+    wav_output_filename = 'test1.wav'  # name of .wav file
+
+    # check input devices
+    audio = pyaudio.PyAudio()
+    info = audio.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+    ###################
+    # GET AUDIO STREAM
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=samp_rate, input=True, frames_per_buffer=chunk,
+                        input_device_index=dev_index)
+    frames = []
+    for ii in range(0, int((samp_rate / chunk) * record_secs)):
+        data = stream.read(chunk)
+        frames.append(data)
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wavefile = wave.open(wav_output_filename, 'wb')
+    wavefile.setnchannels(1)
+    wavefile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))  # 16-bit resolution
+    wavefile.setframerate(samp_rate)
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+
+    x_audio = get_audio_features(wavefile)
+    x_audio = scaler.transform(x_audio)
+    x_audio = np.expand_dims(x_audio, axis=2)
+    pred = model_audio.predict(x_audio)
+
+    return pred
