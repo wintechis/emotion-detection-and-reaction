4dd5fb41511457d57b3d1934b0fe03fc3b492097
ronjarehmdat <99737582+ronjarehmdat@users.noreply.github.com>
2022-08-01 21:30:12 +0200

    prediction in json for video works now

--- webapp/video_recognizer.py
+++ webapp/video_recognizer.py
@@ -1,8 +1,19 @@
+import json
+import time
+
 import cv2
 import numpy as np
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image
+import json as json
+from json import JSONEncoder
+import numpy
+
+import codecs
+
+
 import keras
+
 global predictions
 
 model = keras.models.load_model('models/model_8_50epoch80_CK48dataset.h5')
@@ -13,8 +24,10 @@ except Exception:
     st.write("Error loading cascade classifiers")
 camera = cv2.VideoCapture(0)
 
+
 def sendPredictions(predictions):
     return predictions
+
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -23,12 +36,14 @@ def gen_frames():  # generate frame by frame from camera
         if not success:
             break
         else:
+            frame = np.array(frame)
             gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.1, 4)
 
             for (x, y, w, h) in faces_detected:
                 print('WORKING')
+                frame = np.array(frame)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
                 roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
                 roi_color = frame[y:y + h, x:x + w]
@@ -40,33 +55,49 @@ def gen_frames():  # generate frame by frame from camera
                 else:
                     for (ex, ey, ew, eh) in facess:
                         face_roi = roi_color[ey: ey + eh, ex:ex + ew]  ## cropping the face
-                # img_pixels = image.img_to_array(roi_gray)
-                # img_pixels = np.expand_dims(img_pixels, axis=0)
-                # img_pixels /= 255
+                        # img_pixels = image.img_to_array(roi_gray)
+                        # img_pixels = np.expand_dims(img_pixels, axis=0)
+                        # img_pixels /= 255
                         final_image = cv2.resize(face_roi, (80, 80))
                         final_image = np.expand_dims(final_image, axis=0)  ## need fourth dimension
                         final_image = final_image / 255.0
 
-                print(final_image.shape)
+                    predictions = model.predict(final_image)
+                    pred_list = predictions.tolist()
+                    pred_json = json.dumps(pred_list)
+                    print(pred_json)
+                    with open('./video_prediction.json', 'w') as file:
+                        file.write(pred_json)
+                    print(pred_json)
+                    # predictions_to_json = predictions.tolist()
+                    # file_path= "/video_prediction.json"
+                    # json.dump(predictions_to_json, codecs.open(file_path, 'w', encoding='utf-8'),
+                    #           separators=(',', ':'),
+                    #           sort_keys=True,
+                    #           indent=4)  ### this saves the array in .json format
 
-                predictions = model.predict(final_image)
+                    # sendPredictions(predictions)
 
-                sendPredictions(predictions)
+                    # find max indexed array
 
-                # find max indexed array
+                    max_index = np.argmax(predictions[0])
+                    highest_prediction_value = predictions.max(1) * 100.0
+                    print(highest_prediction_value)
 
-                max_index = np.argmax(predictions[0])
-
-                emotions = ['angry', 'happy', 'fear', 'sad']
-                predicted_emotion = emotions[max_index]
-                print(predicted_emotion)
-                cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    emotions = ['angry', 'fear', 'happy', 'sad']
+                    predicted_emotion = emotions[max_index]
+                    print(predicted_emotion)
+                    print(emotions[max_index])
+                    display_percentage_of_emotion = str(predicted_emotion) + ": " + str(highest_prediction_value)
+                    print(display_percentage_of_emotion)
+                    cv2.putText(frame, display_percentage_of_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2)
 
             resized_img = cv2.resize(frame, (1000, 700))
 
             ret, buffer = cv2.imencode('.jpg', frame)
 
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
+            yield (
+                    b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
